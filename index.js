f216a30735b2fb1351c5075ac54ee7688b38d283
Wamuyu <michellewachira25@gmail.com>
2022-11-09 20:46:51 +0300

    Reinitialized repository and added files

---
+++ index.js
@@ -0,0 +1,20 @@
+/*let firstName= String;
+let lastName= String;
+
+function namingNames(firstName, lastName){
+    console.log(`My name is ${firstName} ${lastName}`)
+}
+namingNames(`Wamuyu`, `Wachira`);
+namingNames(`Wangechi`,`Tiren`);
+
+var x= 10;
+var y=20;
+var z = x + y;
+document.write(z);
+console.log(z)
+
+let data =200;
+function a(){
+    document.write(data);
+}
+*/
