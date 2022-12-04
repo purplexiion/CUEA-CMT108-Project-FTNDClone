a7f541ef1438f2c2cc7cb19a43d5bcff29201c1c
Wamuyu <michellewachira25@gmail.com>
2022-12-04 07:32:06 +0300

    First Phase of Diamika done and launched

---
+++ index.js
@@ -0,0 +1,8 @@
+//making a submit for the contact us forms
+function msg(){
+   alert("Thank you for filling the form, we will get back to you at the shortest time possible");
+  } 
+
+  function externalMsg(){
+    alert("You will be redirected to an external link")
+  }
